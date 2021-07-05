e721a401ad1f6df616b9ae5dbf57133a0680ff7d
Shoaib Shaukat <SShaukat@pattersoncompanies.com>
2021-07-05 10:56:53 -0500

    npm auditConversion to Angular 12

--- src/app/shared/components/add-post/add-post.component.spec.ts
+++ src/app/shared/components/add-post/add-post.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AddPostComponent } from './add-post.component';
 
@@ -6,7 +6,7 @@ describe('AddPostComponent', () => {
   let component: AddPostComponent;
   let fixture: ComponentFixture<AddPostComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AddPostComponent ]
     })
